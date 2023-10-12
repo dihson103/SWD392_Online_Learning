f8f7413ef77c4d98398d9bbcd4c6195872d4ce0c
Nguyễn Đình Sơn <120614021+dihson103@users.noreply.github.com>
2023-10-12 22:30:20 +0700

    fix swagger authentication config

--- Back-end/OnlineLearning/src/main/java/com/dihson103/onlinelearning/configs/SwaggerConfig.java
+++ Back-end/OnlineLearning/src/main/java/com/dihson103/onlinelearning/configs/SwaggerConfig.java
@@ -1,78 +1,19 @@
 package com.dihson103.onlinelearning.configs;
 
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Configuration
 //@EnableSwagger2
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class SwaggerConfig {
-    public static final String AUTHORIZATION_HEADER = "Authorization";
-
-    private ApiKey apiKey() {
-        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
-    }
-
-    private List<SecurityContext> securityContexts() {
-        return Arrays.asList(
-                SecurityContext.builder()
-                        .securityReferences(defaultAuth())
-                        .build()
-        );
-    }
-
-    private List<SecurityReference> defaultAuth() {
-        AuthorizationScope scope = new AuthorizationScope("global", "accessEverything");
-
-        return Arrays.asList(new SecurityReference("JWT", new AuthorizationScope[]{scope}));
-    }
-
-    @Bean
-    public Docket adminApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("admin-api")
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.dihson103.onlinelearning.controllers"))
-                .paths(PathSelectors.any())
-                .build()
-                .securityContexts(securityContexts())
-                .securitySchemes(Arrays.asList(apiKey()));
-    }
-
-    @Bean
-    public Docket publicApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("public-api")
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.dihson103.onlinelearning.controllers"))
-                .paths(PathSelectors.any())
-                .build()
-                .securityContexts(securityContexts())
-                .securitySchemes(Arrays.asList(apiKey()));
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Blogging API")
-                .description("Blogging API reference for developers")
-                .version("1.0.0")
-                .license("Apache 2.0")
-                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
-                .build();
-    }
 }
